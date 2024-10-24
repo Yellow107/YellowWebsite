36b1a7ebf1882c178ad5731c836a385e5745481c
Hitesh Bhardwaj <bhardwajhitesh09@gmail.com>
2024-10-24 19:27:27 +0530

    Update - 24-10-24

--- src/components/Whatsapp/index.js
+++ src/components/Whatsapp/index.js
@@ -14,9 +14,10 @@ export default function Whatsapp() {
     }
 
     return (
-        <div className="fixed bottom-2 right-2 h-fit w-fit fadeup group z-[10] bg-white/25 backdrop-blur-md rounded-full overflow-hidden">
+        <div className="fixed bottom-3 right-3 h-fit w-fit fadeup group z-[10] bg-white/50 backdrop-blur-sm rounded-full overflow-hidden">
             <Link
                 className="w-full h-full"
+                target="_blank"
                 aria-label="Connect With Us"
                 href="https://wa.me/971545178971"
                 onMouseEnter={handleMouseEnter}
@@ -25,7 +26,7 @@ export default function Whatsapp() {
                 <Image
                     width={120}
                     height={120}
-                    className='animate-spin transition-transform object-cover duration-5000 w-[7vw] h-[7vw] tablet:w-[12vw] tablet:h-[12vw] mobile:w-[20vw] mobile:h-[20vw]'
+                    className='animate-spin transition-transform object-cover duration-5000 w-[6vw] h-[6vw] tablet:w-[12vw] tablet:h-[12vw] mobile:w-[20vw] mobile:h-[20vw]'
                     alt='PopUp Form HireUs Image'
                     src="/assets/icons/talk-to-us-1.svg"
                     priority={false}
@@ -35,7 +36,7 @@ export default function Whatsapp() {
                     height={50}
                     width={50}
                     alt="Whatsapp Icon"
-                    className='absolute top-1/2 left-1/2 -translate-x-1/2 opacity-90 -translate-y-1/2 w-[40%] h-auto group-hover:scale-110 duration-300'
+                    className='absolute rounded-full top-1/2 left-1/2 -translate-x-1/2 opacity-90 -translate-y-1/2 w-[40%] h-auto group-hover:scale-110 duration-300'
                     src={isHovered ? '/assets/icons/whatsapp.gif' : '/assets/icons/hireus.png'}
                 />
             </Link>
