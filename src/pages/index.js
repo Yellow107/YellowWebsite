10be814f7a1f648548749542caf5018302e017f2
Harshgoyal-WebDev <138307903+Harshgoyal-WebDev@users.noreply.github.com>
2025-02-08 12:43:35 +0530

    update-(harsh)
    content update

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,7 @@ import MetaData from "@/components/Metadata";
 import dynamic from 'next/dynamic';
 import AboutUs from "@/components/HomePage/AboutUs";
 import Clients from "@/components/HomePage/Clients";
+import Faq from "@/components/HomePage/Faq";
 
 // Dynamically import Blogs component
 const Blogs = dynamic(() => import('@/components/HomePage/Blogs'));
@@ -17,8 +18,8 @@ const Services = dynamic(()=>import ('@/components/HomePage/Services'), { ssr: f
 export default function Home({ recentPosts }) {
 
   const metadata = {
-    title: "Branding & Communication Agency in Dubai - Yellow Agency",
-    description: "Welcome to Yellow: your trusted branding, marketing, & design agency in Dubai. We specialize in crafting brand stories & innovative marketing strategies. Let your brand shine with expert services. Contact today!",
+    title: "Yellow: Branding & Advertising Agency in Dubai, UAE",
+    description: "Yellow is a leading branding, creative design & advertising agency serving Dubai, Abu Dhabi & GCC. We provide brand strategy, naming, identity and marketing services that achieve our client's objectives.",
     img: "home.png",
     slug: "",
     date_published: "2017-10-22T06:17",
@@ -43,6 +44,7 @@ export default function Home({ recentPosts }) {
         <Services />
         <Clients />
         <Blogs posts={recentPosts} />
+        <Faq/>
       </Layout>
     </>
   );
